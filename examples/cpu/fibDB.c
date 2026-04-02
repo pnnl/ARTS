4eb14f09c56c2b9dff51f5faa37cc99b38dbafd4
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-04-02 13:23:21 -0700

    Rewrite fibDB.c

--- examples/cpu/fibDB.c
+++ examples/cpu/fibDB.c
@@ -37,38 +37,36 @@
 ** License for the specific language governing permissions and limitations   **
 ******************************************************************************/
 #include <assert.h>
+#include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "arts.h"
-#include "arts/memory/db.h"
-
-// #define ARTS_USE_CXL 1
 
 uint64_t start = 0;
 
-void fib_join(uint32_t paramc, const uint64_t *paramv, uint32_t depc,
+void fib_join(uint32_t paramc, uint64_t *paramv, uint32_t depc,
               arts_edt_dep_t depv[]) {
-  (void)paramc;
-  (void)depc;
-
-  /* Validate DB pointers before accessing them */
+  // Validate DB pointers before accessing them
   assert(depv[0].ptr != NULL && "First DB pointer is valid");
   assert(depv[1].ptr != NULL && "Second DB pointer is valid");
 
-  /* Extract values from DBs */
-  unsigned int x = *(unsigned int *)depv[0].ptr;
-  unsigned int y = *(unsigned int *)depv[1].ptr;
+  // Extract values from DBs
+  int *x_ptr = depv[0].ptr;
+  int *y_ptr = depv[1].ptr;
+  unsigned int x = *x_ptr;
+  unsigned int y = *y_ptr;
 
-  /* Create a DB to store the result */
-  unsigned int *result_ptr;
+  // Create a DB to store the result
+  int *result_ptr;
 #if ARTS_USE_CXL
-  arts_guid_t result_guid = arts_db_create((void **)&result_ptr,
-                                           sizeof(unsigned int),
-                                           ARTS_DB_CXL, NULL);
+  arts_guid_t result_guid =
+      arts_db_create((void **)&result_ptr, sizeof(unsigned int), ARTS_DB_CXL, NULL);
 #else
-  arts_guid_t result_guid = arts_db_create((void **)&result_ptr,
-                                           sizeof(unsigned int),
-                                           ARTS_DB_DEFAULT, NULL);
+  arts_guid_t result_guid =
+      arts_db_create((void **)&result_ptr, sizeof(unsigned int), ARTS_DB_READ, NULL);
 #endif /* ARTS_USE_CXL */
   assert(result_ptr && "Result ptr not NULL");
   *result_ptr = x + y;
@@ -76,43 +74,37 @@ void fib_join(uint32_t paramc, const uint64_t *paramv, uint32_t depc,
 #if ARTS_USE_CXL
   arts_cxl_producer_flush(result_guid);
 #endif /* ARTS_USE_CXL */
-  /* Signal the parent EDT with the result DB */
-  arts_signal_edt((arts_guid_t)paramv[0], (uint32_t)paramv[1], result_guid,
-                  DB_MODE_RO);
+  // Signal the parent EDT with the result DB
+  arts_signal_edt(paramv[0], paramv[1], result_guid, DB_MODE_READ);
 }
 
-void fib_fork(uint32_t paramc, const uint64_t *paramv, uint32_t depc,
+void fib_fork(uint32_t paramc, uint64_t *paramv, uint32_t depc,
               arts_edt_dep_t depv[]) {
-  (void)depc;
+  unsigned int next = (arts_get_current_node() + 1) % arts_get_total_nodes();
+  // unsigned int next = arts_get_current_node();
 
-  unsigned int next =
-      (arts_get_current_node() + 1) % arts_get_total_nodes();
-
-  arts_guid_t guid = (arts_guid_t)paramv[0];
-  unsigned int slot = (unsigned int)paramv[1];
+  arts_guid_t guid = paramv[0];
+  unsigned int slot = paramv[1];
 
   assert(depv[0].ptr && "depv[0] ptr not null");
-  /* Get the input number from DB */
-  unsigned int num = *(unsigned int *)depv[0].ptr;
+  // Get the input number from DB
+  unsigned int num = ((unsigned int *)(depv[0].ptr))[0];
 
   if (num < 2) {
-    arts_signal_edt(guid, slot, depv[0].guid, DB_MODE_RO);
+    arts_signal_edt(guid, slot, depv[0].guid, DB_MODE_READ);
   } else {
-    /* Create a join EDT that will combine results */
+    // Create a join EDT that will combine results
     arts_guid_t join_guid =
         arts_edt_create(fib_join, paramc, paramv, 2,
                         &(arts_hint_t){.route = arts_get_current_node()});
-
-    /* Create DB for n-1 */
-    unsigned int *n1_ptr;
+    // Create DBs for n-1 and n-2
+    int *n1_ptr;
 #if ARTS_USE_CXL
-    arts_guid_t n1_guid = arts_db_create((void **)&n1_ptr,
-                                         sizeof(unsigned int),
-                                         ARTS_DB_CXL, NULL);
+    arts_guid_t n1_guid =
+        arts_db_create((void **)&n1_ptr, sizeof(unsigned int), ARTS_DB_CXL, NULL);
 #else
-    arts_guid_t n1_guid = arts_db_create((void **)&n1_ptr,
-                                         sizeof(unsigned int),
-                                         ARTS_DB_DEFAULT, NULL);
+    arts_guid_t n1_guid =
+        arts_db_create((void **)&n1_ptr, sizeof(unsigned int), ARTS_DB_READ, NULL);
 #endif /* ARTS_USE_CXL */
     assert(n1_ptr && "n1_ptr not NULL");
     *n1_ptr = num - 1;
@@ -120,16 +112,13 @@ void fib_fork(uint32_t paramc, const uint64_t *paramv, uint32_t depc,
     arts_cxl_producer_flush(n1_guid);
 #endif /* ARTS_USE_CXL */
 
-    /* Create DB for n-2 */
-    unsigned int *n2_ptr;
+    int *n2_ptr;
 #if ARTS_USE_CXL
-    arts_guid_t n2_guid = arts_db_create((void **)&n2_ptr,
-                                         sizeof(unsigned int),
-                                         ARTS_DB_CXL, NULL);
+    arts_guid_t n2_guid =
+        arts_db_create((void **)&n2_ptr, sizeof(unsigned int), ARTS_DB_CXL, NULL);
 #else
-    arts_guid_t n2_guid = arts_db_create((void **)&n2_ptr,
-                                         sizeof(unsigned int),
-                                         ARTS_DB_DEFAULT, NULL);
+    arts_guid_t n2_guid =
+        arts_db_create((void **)&n2_ptr, sizeof(unsigned int), ARTS_DB_READ, NULL);
 #endif /* ARTS_USE_CXL */
     assert(n2_ptr && "n2_ptr not NULL");
     *n2_ptr = num - 2;
@@ -137,76 +126,75 @@ void fib_fork(uint32_t paramc, const uint64_t *paramv, uint32_t depc,
     arts_cxl_producer_flush(n2_guid);
 #endif /* ARTS_USE_CXL */
 
-    /* Create first child task with n-1 */
-    uint64_t args1[2] = {(uint64_t)join_guid, 0};
-    arts_guid_t fib1 =
-        arts_edt_create(fib_fork, 2, args1, 1, &(arts_hint_t){.route = next});
-    arts_signal_edt(fib1, 0, n1_guid, DB_MODE_RO);
+    // Create first child task with n-1
+    uint64_t args1[2] = {join_guid, 0}; // Last param not used since we pass DB
+    arts_guid_t fib1 = arts_edt_create(fib_fork, 2, args1, 1,
+                                       &(arts_hint_t){.route = next});
+    arts_signal_edt(fib1, 0, n1_guid, DB_MODE_READ);
 
-    /* Create second child task with n-2 */
-    uint64_t args2[2] = {(uint64_t)join_guid, 1};
-    arts_guid_t fib2 =
-        arts_edt_create(fib_fork, 2, args2, 1, &(arts_hint_t){.route = next});
-    arts_signal_edt(fib2, 0, n2_guid, DB_MODE_RO);
+    // Create second child task with n-2
+    uint64_t args2[2] = {join_guid, 1}; // Last param not used since we pass DB
+    arts_guid_t fib2 = arts_edt_create(fib_fork, 2, args2, 1,
+                                       &(arts_hint_t){.route = next});
+    arts_signal_edt(fib2, 0, n2_guid, DB_MODE_READ);
   }
 }
 
-void fib_done(uint32_t paramc, const uint64_t *paramv, uint32_t depc,
+void fib_done(uint32_t paramc, uint64_t *paramv, uint32_t depc,
               arts_edt_dep_t depv[]) {
-  (void)depc;
   uint64_t time = arts_get_time_stamp() - start;
 
-  /* Extract result from DB */
-  unsigned int *result_ptr = depv[0].ptr;
+  // Extract result from DB
+  int *result_ptr = depv[0].ptr;
   assert(result_ptr && "fib_done result_ptr not NULL");
   unsigned int result = *result_ptr;
 
-  arts_printf("Fib %u: %u time: %lu nodes: %u workers: %u\n",
-              (unsigned int)paramv[0], result, time,
-              arts_get_total_nodes(), arts_get_total_workers());
+  arts_printf("Fib %u: %u time: %lu nodes: %u workers: %u\n", paramv[0],
+              result, time, arts_get_total_nodes(), arts_get_total_workers());
   arts_shutdown();
 }
 
-void main_edt(uint32_t paramc, const uint64_t *paramv, uint32_t depc,
-              arts_edt_dep_t depv[]) {
-  (void)paramc;
-  (void)depc;
-  (void)depv;
+void init_per_node(unsigned int node_id, int argc, char **argv) {
+  // Nothing to do here
+}
 
-  char **argv = (char **)paramv[1];
-  unsigned int num = (unsigned int)strtoul(argv[1], NULL, 10);
+void init_per_worker(unsigned int node_id, unsigned int worker_id, int argc,
+                     char **argv) {
+  if (!node_id && !worker_id) {
+    unsigned int num = atoi(argv[1]);
 
-  /* Create the done EDT that will receive the final result */
-  arts_guid_t done_guid =
-      arts_edt_create(fib_done, 1, (uint64_t *)&num, 1,
-                      &(arts_hint_t){.route = 0});
+    // Create the done EDT that will receive the final result
+    arts_guid_t done_guid = arts_edt_create(fib_done, 1, (uint64_t *)&num, 1,
+                                            &(arts_hint_t){.route = 0});
 
-  /* Create a DB for the input number */
-  unsigned int *num_ptr;
+    // Create a DB for the input number
+    int *num_ptr;
 #if ARTS_USE_CXL
-  arts_guid_t num_guid = arts_db_create((void **)&num_ptr,
-                                        sizeof(unsigned int),
-                                        ARTS_DB_CXL, NULL);
+    arts_guid_t num_guid =
+        arts_db_create((void **)&num_ptr, sizeof(unsigned int), ARTS_DB_CXL, NULL);
+// arts_cxl_producer_flush(num_guid);
 #else
-  arts_guid_t num_guid = arts_db_create((void **)&num_ptr,
-                                        sizeof(unsigned int),
-                                        ARTS_DB_DEFAULT, NULL);
+    arts_guid_t num_guid =
+        arts_db_create((void **)&num_ptr, sizeof(unsigned int), ARTS_DB_READ, NULL);
 #endif /* ARTS_USE_CXL */
-  assert(num_ptr && "main_edt num_ptr not NULL");
-  *num_ptr = num;
+    assert(num_ptr && "init_per_worker num_ptr not NULL");
+    *num_ptr = num;
 #if ARTS_USE_CXL
-  arts_cxl_producer_flush(num_guid);
+    arts_cxl_producer_flush(num_guid);
 #endif /* ARTS_USE_CXL */
 
-  /* Start the computation */
-  uint64_t args[2] = {(uint64_t)done_guid, 0};
-  start = arts_get_time_stamp();
-  arts_guid_t fib_guid =
-      arts_edt_create(fib_fork, 2, args, 1, &(arts_hint_t){.route = 0});
-  arts_signal_edt(fib_guid, 0, num_guid, DB_MODE_RO);
+    // Start the computation
+    uint64_t args[2] = {done_guid, 0}; // Last param not used since we pass DB
+    start = arts_get_time_stamp();
+    arts_guid_t fib_guid = arts_edt_create(fib_fork, 2, args, 1,
+                                           &(arts_hint_t){.route = 0});
+    arts_signal_edt(fib_guid, 0, num_guid, DB_MODE_READ);
+  }
 }
 
 int main(int argc, char **argv) {
+  // catch_all_signals();
+  arts_printf("Starting ArtsRT with DB-based Fibonacci\n");
   arts_rt(argc, argv);
   return 0;
 }
