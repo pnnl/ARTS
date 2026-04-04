54d5df164138b1b049da9c6089abc406408e8528
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-04-04 08:45:01 -0700

    FibDB test scripts

--- run_fibDB.sh
+++ run_fibDB.sh
@@ -4,4 +4,8 @@ cd build/examples/cpu
 cp ../../../run_cxl.sh .
 cp ../../../arts.cfg .
 chmod +x run_cxl.sh
-./run_cxl.sh ./fibDB "$1"
+for i in {1..100}; do
+  ./run_cxl.sh ./fibDB 25 &> fibDB_out_${i}.log
+done
+cd ../../../
+python3 check_logs_fibDB.py
