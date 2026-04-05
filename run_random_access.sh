b0475c8802f5438e9a3a6a2d9704e882b31af113
Akhil Reddy Alasandagutti <akhilreddy.alasandagutti@pnnl.gov>
2026-04-05 12:37:07 -0700

    Remove Crete updates

--- run_random_access.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd build/examples/cpu
-cp ../../../run_cxl.sh .
-cp ../../../arts.cfg .
-chmod +x run_cxl.sh
-cp random_access/random_access RANDOM_ACCESS
-./run_cxl.sh ./RANDOM_ACCESS
--- run_stream.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd build/examples/cpu
-cp ../../../run_cxl.sh .
-cp ../../../arts.cfg .
-chmod +x run_cxl.sh
-cp stream/stream STREAM
-./run_cxl.sh ./STREAM 131072
